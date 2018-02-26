54fc03ea6365408a3f52eed58b1fcdc03f66d30e
Katia Romaniuk <katia-l@mail.ru>
2018-02-26 10:03:12 +0300

    pass 31 test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    let obj ={};
+    for (let i=0; i<array.length; i++) {
+        obj[array[i]] = true;
+    }
+    let longest1 = 0;
+    let longest2 = 0;
+    for (let i=1; i<=array.length+1; i++) {
+        if (!obj[i]) {
+            if (longest2 > longest1) longest1 = longest2;
+            longest2 = 0;
+        } else {
+            longest2++;
+        }
+    }
+    return longest1;
 }
